1f22879eb1be750e8cc93e3b1ab6bc946978682e
Gauthier Leonard <gauthier@e36knots.com>
2024-11-13 19:59:02 +0100

    feat(acp99): adjust ACP77WarpMessengerTestMock

--- contracts/src/mocks/ACP77WarpMessengerTestMock.sol
+++ contracts/src/mocks/ACP77WarpMessengerTestMock.sol
@@ -32,19 +32,16 @@ contract ACP77WarpMessengerTestMock {
         bytes(hex"3456781234567812345678123456781234567812345678123456781234567812");
     bytes32 private constant DEFAULT_MESSAGE_ID =
         0x39fa07214dc7ff1d2f8b6dfe6cd26f6b138ee9d40d013724382a5c539c8641e2;
-    address private constant DEFAULT_VALIDATOR_MANAGER_ADDRESS =
-        0xf06FD5A15c8333CcC2b336D72ECE381c88cB657f;
     bytes32 private constant DEFAULT_VALIDATION_ID =
-        0x6bc851f1cf9fe68ddb8c6fe4b72f467aeeff662677d4d65e1a387085bfdda283;
-
+        0x3a41d4db60b49389d4b121c2137a1382431a89369c5445c2a46877c3929dd9c6;
     uint64 private constant DEFAULT_VALIDATION_UPTIME_SECONDS = uint64(2_544_480);
 
-    address private immutable tokenHomeAddress;
-    address private immutable tokenRemoteAddress;
+    address private immutable validatorManagerAddress;
 
-    constructor(address tokenHomeAddress_, address tokenRemoteAddress_) {
-        tokenHomeAddress = tokenHomeAddress_;
-        tokenRemoteAddress = tokenRemoteAddress_;
+    constructor(
+        address _validatorManagerAddress
+    ) {
+        validatorManagerAddress = _validatorManagerAddress;
     }
 
     function getBlockchainID() external pure returns (bytes32) {
@@ -60,13 +57,14 @@ contract ACP77WarpMessengerTestMock {
     // Mocks valid warp messages for testing
     // messageIndex = 1: RegisterRemoteMessage used for AvalancheICTTRouter tests
     // messageIndex = 2: InitializeValidatorSetMessage used for ACP99Manager tests
-    // messageIndex = 3: SubnetValidatorRegistrationMessage used for ACP99Manager tests
+    // messageIndex = 3: ValidatorRegistrationMessage used for ACP99Manager tests
     // messageIndex = 4: ValidatorUptimeMessage used for ACP99Manager tests
     // messageIndex = 5: ValidatorWeightUpdateMessage used for ACP99Manager tests (weight = 200)
     // messageIndex = 6: ValidatorWeightUpdateMessage used for ACP99Manager tests (weight = 0)
+    // messageIndex = 7: ValidatorRegistrationMessage used for ACP99Manager tests (expired)
     function getVerifiedWarpMessage(
         uint32 messageIndex
-    ) external pure returns (WarpMessage memory message, bool valid) {
+    ) external view returns (WarpMessage memory message, bool valid) {
         if (messageIndex == 2) {
             return _initializeValidatorSetWarpMessage();
         } else if (messageIndex == 3) {
@@ -77,19 +75,21 @@ contract ACP77WarpMessengerTestMock {
             return _validatorWeightUpdateWarpMessage();
         } else if (messageIndex == 6) {
             return _validatorWeightZeroWarpMessage();
+        } else if (messageIndex == 7) {
+            return _validatorRegistrationExpiredWarpMessage();
         }
     }
 
-    function _initializeValidatorSetWarpMessage() private pure returns (WarpMessage memory, bool) {
+    function _initializeValidatorSetWarpMessage() private view returns (WarpMessage memory, bool) {
         InitialValidator[] memory initialValidators = new InitialValidator[](2);
         initialValidators[0] =
-            InitialValidator({nodeID: DEFAULT_NODE_ID_02, weight: 100, blsPublicKey: new bytes(48)});
+            InitialValidator({nodeID: DEFAULT_NODE_ID_02, weight: 180, blsPublicKey: new bytes(48)});
         initialValidators[1] =
-            InitialValidator({nodeID: DEFAULT_NODE_ID_03, weight: 100, blsPublicKey: new bytes(48)});
+            InitialValidator({nodeID: DEFAULT_NODE_ID_03, weight: 20, blsPublicKey: new bytes(48)});
         ConversionData memory conversionData = ConversionData({
             subnetID: DEFAULT_SUBNET_ID,
             validatorManagerBlockchainID: ANVIL_CHAIN_ID_HEX,
-            validatorManagerAddress: DEFAULT_VALIDATOR_MANAGER_ADDRESS,
+            validatorManagerAddress: validatorManagerAddress,
             initialValidators: initialValidators
         });
         bytes memory encodedConversion = ValidatorMessages.packConversionData(conversionData);
@@ -147,7 +147,7 @@ contract ACP77WarpMessengerTestMock {
                 ValidatorMessages.L1_VALIDATOR_WEIGHT_MESSAGE_TYPE_ID,
                 DEFAULT_VALIDATION_ID,
                 uint64(1),
-                uint64(200)
+                uint64(40)
             )
         });
 
@@ -164,6 +164,25 @@ contract ACP77WarpMessengerTestMock {
                 DEFAULT_VALIDATION_ID,
                 uint64(1),
                 uint64(0)
+            )
+        });
+
+        return (warpMessage, true);
+    }
+
+    function _validatorRegistrationExpiredWarpMessage()
+        private
+        pure
+        returns (WarpMessage memory, bool)
+    {
+        WarpMessage memory warpMessage = WarpMessage({
+            sourceChainID: P_CHAIN_ID_HEX,
+            originSenderAddress: address(0),
+            payload: abi.encodePacked(
+                ValidatorMessages.CODEC_ID,
+                ValidatorMessages.L1_VALIDATOR_REGISTRATION_MESSAGE_TYPE_ID,
+                DEFAULT_VALIDATION_ID,
+                false
             )
         });
 
