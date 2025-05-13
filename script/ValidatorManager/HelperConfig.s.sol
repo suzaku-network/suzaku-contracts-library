00b082d09d53ceaee4cc40b1304ca41b78425746
GENGE <gonzaloetjo@gmail.com>
2025-05-13 19:41:20 +0200

    fix: warning pure

--- script/ValidatorManager/HelperConfig.s.sol
+++ script/ValidatorManager/HelperConfig.s.sol
@@ -24,7 +24,7 @@ contract HelperConfig is Script {
         // }
     }
 
-    function getOrCreateAnvilConfig() public returns (NetworkConfig memory) {
+    function getOrCreateAnvilConfig() public pure returns (NetworkConfig memory) {
         uint256 proxyAdminOwnerKey =
             0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
         uint256 validatorManagerOwnerKey =
